352b08ac6c09037adf023a96b8e72f711174de0b
Jonathan <heyjonadice@gmail.com>
2024-05-11 21:00:27 -0600

    primer commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Inicializar el slider de imágenes
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
